0e3a82cef9de5b0ecc13c17895c3db3bf5163e75
Amin Vahdani <amin.vahdani@payever.org>
2025-05-18 23:12:22 +0330

    Create user creation request dto and global validation pipe in main

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 const config = new ConfigService();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
   await app.listen(config.get<number>('PORT'));
 }
 bootstrap();
