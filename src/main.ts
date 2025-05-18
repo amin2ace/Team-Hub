710a24e6b34db9b7818e9d6bb24d583e690e6a77
Amin Vahdani <amin.vahdani@payever.org>
2025-05-18 23:07:07 +0330

    Clear port hard code

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
+const config = new ConfigService();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(config.get<number>('PORT'));
 }
 bootstrap();
