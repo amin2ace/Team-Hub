49911eef9e2967f56bc9cb9c97c67d2f81dbac52
Amin Vahdani <amin.vahdani@payever.org>
2025-05-21 20:53:57 +0330

    Implement CRUD operations in UsersController for user management

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,31 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Patch,
+  Delete,
+  Param,
+  Body,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
+import { UserCreateDto, UserUpdateDto } from './dto';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @Post('/')
+  async create(@Body() createUserDto: UserCreateDto) {}
+
+  @Get('/all')
+  async findAll() {}
+
+  @Get('/:id')
+  async findOne(@Param('id') id: string) {}
+
+  @Patch('/update/:id')
+  async update(@Param('id') id: string, @Body() updateUserDto: UserUpdateDto) {}
+
+  @Delete('/delete:id')
+  async remove(@Param('id') id: string) {}
 }
