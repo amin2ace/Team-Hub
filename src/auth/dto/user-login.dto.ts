e9aedfeb9236ef6407a03db23ef53d7c7db1f1aa
Amin Vahdani <amin.vahdani@payever.org>
2025-05-21 23:24:53 +0330

    Add validation decorators to userLoginDto for email and password fields

--- src/auth/dto/user-login.dto.ts
+++ src/auth/dto/user-login.dto.ts
@@ -1,3 +1,12 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 import { UserCreateDto } from 'src/users/dto/create-user-request.dto';
 
-export class userLoginDto implements Partial<UserCreateDto> {}
+export class userLoginDto implements Partial<UserCreateDto> {
+  @IsEmail()
+  @IsNotEmpty()
+  email?: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password?: string;
+}
