ce02ff358ecc9b8e5b78eaac23c5575be18c979d
Amin Vahdani <amin.vahdani@payever.org>
2025-05-21 23:35:57 +0330

    Change location of  userLoginResponseDto class

--- src/auth/dto/index.ts
+++ src/auth/dto/index.ts
@@ -1,5 +1,6 @@
 export * from './forget-password.dto';
 export * from './user-login.dto';
+export * from './user-login-response.dto';
 export * from './reset-password.dto';
 export * from './refresh-token.dto';
 export * from './token-response.dto';
