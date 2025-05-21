262771d290d3510fb82f0889df0805804b8aeb63
Amin Vahdani <amin.vahdani@payever.org>
2025-05-22 01:22:48 +0330

    Refactor userLoginResponseDto to include tokens property of type IToken[]

--- src/auth/dto/user-login-response.dto.ts
+++ src/auth/dto/user-login-response.dto.ts
@@ -1,1 +1,7 @@
-export class userLoginResponseDto {}
+import { Expose } from 'class-transformer';
+import { IToken } from 'src/common/enum';
+
+export class userLoginResponseDto {
+  @Expose()
+  tokens: IToken[];
+}
