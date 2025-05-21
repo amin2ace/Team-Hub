b74721fb35861ce28cfc09217bef7191734d3040
Amin Vahdani <amin.vahdani@payever.org>
2025-05-21 20:14:10 +0330

    Implement authentication endpoints for user registration, login, refresh, password management, and logout

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Req } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { UserCreateDto } from 'src/users/dto/create-user-request.dto';
+import { ForgetPasswordDto, ResetPasswordDto, userLoginDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post('/register')
+  async register(@Body() registerData: UserCreateDto) {}
+
+  @Post('/login')
+  async login(@Body() loginData: userLoginDto) {}
+
+  @Post('/refresh')
+  async refresh() {}
+
+  @Post('/forget')
+  async forget(@Body() forgetPasswordData: ForgetPasswordDto) {}
+
+  @Post('/reset')
+  async reset(@Body() resetPasswordData: ResetPasswordDto) {}
+
+  @Post('/logout')
+  async logout(@Req() req) {}
 }
