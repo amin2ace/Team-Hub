4b66c566b80344878614c512683e71322a4094d3
Amin Vahdani <amin.vahdani@payever.org>
2025-05-21 21:44:05 +0330

    Refactor AuthModule to import UsersModule instead of UsersService; export UsersService from UsersModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { UsersService } from 'src/users/users.service';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
-  imports: [UsersService],
+  imports: [UsersModule],
   controllers: [AuthController],
   providers: [AuthService],
 })
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -8,5 +8,6 @@ import { User } from './schema/user.schema';
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
   providers: [UsersService],
+  exports: [UsersService],
 })
 export class UsersModule {}
