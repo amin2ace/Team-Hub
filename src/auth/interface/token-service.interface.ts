281df575b2ad76f988d0752788f1ed9ea1496a53
Amin Vahdani <amin.vahdani@payever.org>
2025-05-21 23:44:13 +0330

    Add methods to ITokenService interface for token management

--- src/auth/interface/token-service.interface.ts
+++ src/auth/interface/token-service.interface.ts
@@ -1,1 +1,9 @@
-export interface ITokenService {}
+import { Token, TokenType } from 'src/common/enum';
+
+export interface ITokenService {
+  createToken(type: TokenType): Promise<Token>;
+  verifyToken(token: Token): Promise<Boolean>;
+  storeToken(token: Token): Promise<void>;
+  removeToken(token: Token): Promise<void>;
+  refreshToken(token: Token): Promise<Token>;
+}
