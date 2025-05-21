d44235e85da15072e897b01e4abf69b2279e3198
Amin Vahdani <amin.vahdani@payever.org>
2025-05-21 23:45:03 +0330

    Implement ITokenService interface methods in TokenService

--- src/auth/token.service.ts
+++ src/auth/token.service.ts
@@ -1,11 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
+import { ITokenService } from './interface/token-service.interface';
+import { TokenType, Token } from 'src/common/enum';
 
 @Injectable()
-export class TokenService {
+export class TokenService implements ITokenService {
   constructor(
     private readonly config: ConfigService,
     private readonly jwtService: JwtService,
   ) {}
+  async createToken(type: TokenType): Promise<Token> {
+    throw new Error('Method not implemented.');
+  }
+  async verifyToken(token: Token): Promise<Boolean> {
+    throw new Error('Method not implemented.');
+  }
+  async storeToken(token: Token): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+  async removeToken(token: Token): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+  async refreshToken(token: Token): Promise<Token> {
+    throw new Error('Method not implemented.');
+  }
 }
