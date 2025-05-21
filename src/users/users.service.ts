a9faf410f620b0eef5ca917ca14184fec1037428
Amin Vahdani <amin.vahdani@payever.org>
2025-05-21 23:55:36 +0330

    Implement UsersService methods for user management

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { IUsersService } from './interface/users-service.interface';
+import { UserCreateDto, UserCreateResponseDto, UserUpdateDto } from './dto';
+import { User } from './schema/user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService implements IUsersService {
+  createNewUser(createUserDto: UserCreateDto): Promise<UserCreateResponseDto> {
+    throw new Error('Method not implemented.');
+  }
+  findAll(): Promise<User[]> {
+    throw new Error('Method not implemented.');
+  }
+  findOne(userId: string): Promise<User> {
+    throw new Error('Method not implemented.');
+  }
+  update(userId: string, updateUserData: UserUpdateDto): Promise<any> {
+    throw new Error('Method not implemented.');
+  }
+  remove(userId: string): Promise<any> {
+    throw new Error('Method not implemented.');
+  }
+  findByEmail(email: string): Promise<any | null> {
+    throw new Error('Method not implemented.');
+  }
+  findById(userId: string): Promise<any | null> {
+    throw new Error('Method not implemented.');
+  }
+  updatePassword(userId: string, hashedPassword: string): Promise<any> {
+    throw new Error('Method not implemented.');
+  }
+}
