d1e75d4fbb05700b35ef7b9e0763dfceca14ee41
Amin Vahdani <amin.vahdani@payever.org>
2025-05-21 20:50:11 +0330

    Add UserUpdateDto and UsersService interface for user management operations

--- src/users/dto/index.ts
+++ src/users/dto/index.ts
@@ -1,3 +1,4 @@
 export * from './create-user-request.dto';
 export * from './create-user-response.dto';
 export * from './user-login-response.dto';
+export * from './update-user.dto';
---
+++ src/users/interface/users-service.interface.ts
@@ -0,0 +1,13 @@
+import { UserCreateResponseDto, UserCreateDto, UserUpdateDto } from '../dto';
+import { User } from '../schema/user.schema';
+
+export interface UsersService {
+  createNewUser(createUserDto: UserCreateDto): Promise<UserCreateResponseDto>;
+  findAll(): Promise<User[]>;
+  findOne(userId: string): Promise<User>;
+  update(userId: string, updateUserData: UserUpdateDto): Promise<any>;
+  remove(userId: string): Promise<any>;
+  findByEmail(email: string): Promise<any | null>;
+  findById(userId: string): Promise<any | null>;
+  updatePassword(userId: string, hashedPassword: string): Promise<any>;
+}
