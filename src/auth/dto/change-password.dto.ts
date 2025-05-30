1434fd8aeb5f005e3bde33f54cd61069899ee193
Amin Vahdani <amin.vahdani@payever.org>
2025-05-30 22:10:07 +0330

    Add email validation to ChangePasswordDto

--- src/auth/dto/change-password.dto.ts
+++ src/auth/dto/change-password.dto.ts
@@ -1,4 +1,5 @@
 import {
+  IsEmail,
   IsNotEmpty,
   IsString,
   IsStrongPassword,
@@ -6,6 +7,10 @@ import {
 } from 'class-validator';
 
 export class ChangePasswordDto {
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
   @IsString()
   @IsNotEmpty()
   oldPassword: string;
