a36f99d039d0fafdc41065e40df077e28174b2b6
Amin Vahdani <amin.vahdani@payever.org>
2025-06-01 22:51:06 +0330

    Refactor IUsersService update method to use ObjectId for userId and return User type

--- src/users/interface/users-service.interface.ts
+++ src/users/interface/users-service.interface.ts
@@ -1,11 +1,12 @@
-import { UserCreateResponseDto, UserCreateDto, UserUpdateDto } from '../dto';
+import { ObjectId } from 'typeorm';
+import { UserCreateDto, UserUpdateDto } from '../dto';
 import { User } from '../schema/user.entity';
 
 export interface IUsersService {
   createNewUser(createUserDto: UserCreateDto): Promise<User>;
   findAll(): Promise<User[]>;
   findOne(userId: string): Promise<User>;
-  update(userId: string, updateUserData: UserUpdateDto): Promise<any>;
+  update(userId: ObjectId, updateUserData: UserUpdateDto): Promise<User>;
   remove(userId: string): Promise<any>;
   findByEmail(email: string): Promise<any | null>;
   findById(userId: string): Promise<any | null>;
