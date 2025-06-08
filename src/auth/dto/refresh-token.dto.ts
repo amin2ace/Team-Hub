31906c13250814b6c142cb6ca7743cc6d0d7ad2f
Amin Vahdani <amin.vahdani@payever.org>
2025-06-08 22:29:21 +0330

    Refactor RefreshTokenDto to use refreshToken property with validation decorators for improved data handling

--- src/auth/dto/refresh-token.dto.ts
+++ src/auth/dto/refresh-token.dto.ts
@@ -1,3 +1,9 @@
+import { Expose } from 'class-transformer';
+import { IsNotEmpty, IsString } from 'class-validator';
+
 export class RefreshTokenDto {
-  token: string;
+  @Expose()
+  @IsString()
+  @IsNotEmpty()
+  refreshToken: string;
 }
